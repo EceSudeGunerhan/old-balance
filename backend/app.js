30001bf148c3ff96809ae52087d11985c2b31770
EceSudeGunerhan <149162447+EceSudeGunerhan@users.noreply.github.com>
2024-06-28 23:25:21 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,3 @@
-/*
-
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-*/
-
-//require('./api/models/db');
-//require('./api/models/venue');
-
-// var apiRouter = require('./api/routes/index')
-
-//var app = express();
-
-/*
-
-app
-    .use('/api', apiRouter)
-
-    .use(express.static(path.join(__dirname, 'public')))
-    .use(logger('dev'))
-    .use(express.json())
-    .use(express.urlencoded({ extended: false }))
-    .use(cookieParser())
-
-    .use('/', indexRouter)
-    .use('/users', usersRouter)
-
-module.exports = app;
-
-*/
-
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
