07aba5c67d50ede254efd03b8e1536d507c5616e
EceSudeGunerhan <149162447+EceSudeGunerhan@users.noreply.github.com>
2024-06-28 23:23:59 +0300

    Update index.js

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -1,36 +1,3 @@
-/*
-
-const express = require("express");
-
-const router = express.Router();
-
-const venueController = require("../controllers/venueController")
-const commentController = require('../controllers/commentController')
-
-router
-    .route('/venues')
-    .get(venueController.venuesGetAll)
-    .post(venueController.venuesAddOne)
-
-router
-    .route('/venues/:venueid')
-    .get(venueController.venuesReadOne)
-    .put(venueController.venuesUpdateOne)
-    .delete(venueController.venuesDeleteOne)
-
-router
-    .route('/venues/:venueid/comments')
-    .post(commentController.commentsAddOne)
-
-router
-    .route('/venues/:venueid/comments/:commentid')
-    .get(commentController.commentsReadOne)
-    .put(commentController.commentsUpdateOne)
-    .delete(commentController.commentsDeleteOne)
-
-module.exports = router;
-
-*/
 
 const express = require('express');
 const router = express.Router();
